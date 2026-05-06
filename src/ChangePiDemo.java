18e4556a4153e342430efbbd02cef796a5068688
Shahd <shahad9mubarak@gmail.com>
2026-05-06 16:21:43 +0400

    test 2

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -2,6 +2,8 @@ public class ChangePiDemo {
     static void main(String[] args) {
         ChangePi CH=new ChangePi();
         System.out.println(CH.changePi("xpix"));
+        System.out.println(CH.changePi("pipi"));
+
 
     }
 }

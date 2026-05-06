23ea477eed954cd14b95bd6500e97c9be7f4fb0a
Shahd <shahad9mubarak@gmail.com>
2026-05-06 16:00:30 +0400

    create  Count7Demo  class

--- src/Count7Demo.java
+++ src/Count7Demo.java
@@ -1,5 +1,8 @@
 public class Count7Demo {
     static void main(String[] args) {
-
+        Count7 OBJ=new Count7();
+        System.out.println(OBJ.count7(717));
+        System.out.println(OBJ.count7());
+        System.out.println(OBJ.count7());
     }
 }

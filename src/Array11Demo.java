83454f40dd2767c3dde92f27946f68ccd8de9bdd
Shahd <shahad9mubarak@gmail.com>
2026-05-06 16:34:23 +0400

    test 2

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -2,5 +2,7 @@ public class Array11Demo {
     static void main(String[] args) {
         Array11 AR=new Array11();
         System.out.println(AR.array11(new int[]{1, 2, 11}, 0));
+        System.out.println(AR.array11(new int[]{11, 11}, 0));
+
     }
 }

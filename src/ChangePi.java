df56a15cb68d26b08e0ba8f312f58b5d6dc9eff5
Shahd <shahad9mubarak@gmail.com>
2026-05-06 16:18:55 +0400

    add string var

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -7,7 +7,8 @@ public class ChangePi {
         if (str.startsWith("pi")){
             return "3.14" + changePi(str.substring(2));
         }else {
-            String FRIST = str.charAt(0);
+            String FRIST = String.valueOf(str.charAt(0));
+            return FRIST+changePi(str.substring(1));
 
         }
 

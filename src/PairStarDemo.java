60a4bee0f218ae3ded8d9d3d287b8f412744698f
Shahd <shahad9mubarak@gmail.com>
2026-05-06 16:46:29 +0400

    create OBJECT

--- src/PairStarDemo.java
+++ src/PairStarDemo.java
@@ -2,7 +2,7 @@ public class PairStarDemo {
     static void main(String[] args) {
         PairStar P = new PairStar();
         System.out.println(P.pairStar("hello"));
-        System.out.println(P.pairStar());
+        System.out.println(P.pairStar("xxyy"));
 
         System.out.println(P.pairStar());
 

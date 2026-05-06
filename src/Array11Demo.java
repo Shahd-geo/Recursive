6a0afc8e80688bb440132da6e632415074a14592
Shahd <shahad9mubarak@gmail.com>
2026-05-06 16:35:22 +0400

    Task 6: Create a Recursive Array11 Program in Java

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -3,6 +3,8 @@ public class Array11Demo {
         Array11 AR=new Array11();
         System.out.println(AR.array11(new int[]{1, 2, 11}, 0));
         System.out.println(AR.array11(new int[]{11, 11}, 0));
+        System.out.println(AR.array11(new int[]{1, 2,3,4}, 0));
+
 
     }
 }

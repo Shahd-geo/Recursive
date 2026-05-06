ecc2c198727254f7495e8923f17d480d17d88552
Shahd <shahad9mubarak@gmail.com>
2026-05-06 16:12:08 +0400

    creat object

--- src/CountXDemo.java
+++ src/CountXDemo.java
@@ -2,7 +2,7 @@ public class CountXDemo {
     static void main(String[] args) {
         CountX COUNTER=new CountX();
         System.out.println(COUNTER.countX("xxhixx"));
-        System.out.println(COUNTER.countX(""));
+        System.out.println(COUNTER.countX("xhixhix"));
         System.out.println(COUNTER.countX(""));
 
 

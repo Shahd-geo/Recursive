5dfcf3414a206daf96ee338c85c0b1f167be917e
Shahd <shahad9mubarak@gmail.com>
2026-05-07 09:45:59 +0400

    test 2

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -2,6 +2,8 @@ public class StrCountDemo {
     static void main(String[] args) {
         StrCount obj = new StrCount();
         System.out.println(obj.strCount("catcowcat","cat"));
+        System.out.println(obj.strCount("catcowcat","cow"));
+
 
     }
 }

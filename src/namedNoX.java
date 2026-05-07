0ab93c8493380f549f739ef468e9dd3524dbcc14
Shahd <shahad9mubarak@gmail.com>
2026-05-07 11:21:47 +0400

    create NoXDemo  class

--- src/namedNoX.java
+++ src/namedNoX.java
@@ -1,14 +1,18 @@
 public class namedNoX {
-    public static String noX(String str){
-        if (str.length()==0){
+    public static String noX(String str) {
+        if (str.length() == 0) {
             return "";
         }
         char frist = str.charAt(0);
         String remaining = noX(str.substring(1));
-        if (frist=='x'){
+        if (frist == 'x') {
             return remaining;
 
+        } else {
+            return frist + remaining;
+
         }
+
 
     }
 }

e3c74b7b541dbcb361c0f00874c54a87937185a5
Shahd <shahad9mubarak@gmail.com>
2026-05-07 12:37:59 +0400

    test 1

--- src/StrCopiesDemo.java
+++ src/StrCopiesDemo.java
@@ -1,6 +1,9 @@
 public class StrCopiesDemo {
     static void main(String[] args) {
         StrCopies O = new StrCopies();
+        System.out.println(O.strCopies(("catcowcat", "cat", 2));
+        System.out.println(O.strCopies());
+        System.out.println(O.strCopies());
 
     }
 }

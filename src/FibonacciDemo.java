0aa9173d9969514e315961ad14d57c53ffe1c909
Shahd <shahad9mubarak@gmail.com>
2026-05-07 12:48:23 +0400

    test 2

--- src/FibonacciDemo.java
+++ src/FibonacciDemo.java
@@ -2,7 +2,7 @@ public class FibonacciDemo {
     static void main(String[] args) {
         Fibonacci F = new Fibonacci();
         System.out.println(F.fibonacci(0));
-        System.out.println(F.fibonacci(0));
+        System.out.println(F.fibonacci(1));
         System.out.println(F.fibonacci(0));
 
     }

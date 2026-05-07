7cda05ae0329686b8d681d5634ea92c51ba2214c
Shahd <shahad9mubarak@gmail.com>
2026-05-07 12:38:32 +0400

    test 2

--- src/StrCopiesDemo.java
+++ src/StrCopiesDemo.java
@@ -2,7 +2,7 @@ public class StrCopiesDemo {
     static void main(String[] args) {
         StrCopies O = new StrCopies();
         System.out.println(O.strCopies(("catcowcat", "cat", 2));
-        System.out.println(O.strCopies());
+        System.out.println(O.strCopies("catcowcat", "cow", 2));
         System.out.println(O.strCopies());
 
     }

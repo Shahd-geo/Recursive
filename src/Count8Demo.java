954b02c1df773bc1ab86b846407241da7d758db8
Shahd <shahad9mubarak@gmail.com>
2026-05-07 10:19:20 +0400

    test 2

--- src/Count8Demo.java
+++ src/Count8Demo.java
@@ -2,6 +2,8 @@ public class Count8Demo {
     static void main(String[] args) {
         Count8 OBJ = new Count8();
         System.out.println(OBJ.count8(8));
+        System.out.println(OBJ.count8(818));
+
 
     }
 }

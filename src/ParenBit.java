4698a2db30519e058defd8426345d96935cb3486
Shahd <shahad9mubarak@gmail.com>
2026-05-07 12:25:48 +0400

    If the last character is not ')'

--- src/ParenBit.java
+++ src/ParenBit.java
@@ -4,7 +4,7 @@ public class ParenBit {
             return str;
         }
         if (str.charAt(0)!= '(' ){
-            return parenBit(str.substring(0, str.length() - 1) );
+            return parenBit(str.substring(1));
 
         }
 

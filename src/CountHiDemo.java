dcc860042cf9f1f2b0925ff143aaaecdee2ab47e
Shahd <shahad9mubarak@gmail.com>
2026-05-07 10:30:21 +0400

    test 2

--- src/CountHiDemo.java
+++ src/CountHiDemo.java
@@ -2,6 +2,8 @@ public class CountHiDemo {
     static void main(String[] args) {
         CountHi obj = new CountHi();
         System.out.println(obj.countHi("xxhixx"));
+        System.out.println(obj.countHi("xhixhix"));
+
 
     }
 }

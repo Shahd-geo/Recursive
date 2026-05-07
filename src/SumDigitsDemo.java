99d0d0278f0e2a9d093bc782d7d43152edc61644
Shahd <shahad9mubarak@gmail.com>
2026-05-07 12:55:15 +0400

    test 2

--- src/SumDigitsDemo.java
+++ src/SumDigitsDemo.java
@@ -2,6 +2,8 @@ public class SumDigitsDemo {
     static void main(String[] args) {
         SumDigits O = new SumDigits();
         System.out.println(O.sumDigits(126));
+        System.out.println(O.sumDigits(49));
+        System.out.println(O.sumDigits(126));
 
     }
 }

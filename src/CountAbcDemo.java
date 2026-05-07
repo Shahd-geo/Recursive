e1d174706c219905154f4f252ed639b9569c4408
Shahd <shahad9mubarak@gmail.com>
2026-05-07 08:59:00 +0400

    test 2

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -2,6 +2,10 @@ public class CountAbcDemo {
     static void main(String[] args) {
         CountAbc OBJ = new CountAbc();
         System.out.println(OBJ.countAbc("abc"));
+        System.out.println(OBJ.countAbc("abcxxabc"));
+        System.out.println(OBJ.countAbc("abcxxabc"));
+
+
 
     }
 }

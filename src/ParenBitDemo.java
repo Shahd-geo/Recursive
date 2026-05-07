3f3bb8065db611812e9db8432adfef87fc67a122
Shahd <shahad9mubarak@gmail.com>
2026-05-07 12:30:33 +0400

    test 2

--- src/ParenBitDemo.java
+++ src/ParenBitDemo.java
@@ -2,7 +2,7 @@ public class ParenBitDemo {
     static void main(String[] args) {
         ParenBit P =new ParenBit();
         System.out.println(P.parenBit( "xyz(abc)123"));
-        System.out.println(P.parenBit());
+        System.out.println(P.parenBit( "x(hello)"));
         System.out.println(P.parenBit());
 
     }

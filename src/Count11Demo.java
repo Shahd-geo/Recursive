b319c68f28d021819535eece1823c01e198c8fbf
Shahd <shahad9mubarak@gmail.com>
2026-05-07 12:10:57 +0400

    Task 16: Create a Recursive Count11 Program in Java

--- src/Count11Demo.java
+++ src/Count11Demo.java
@@ -1,2 +1,11 @@
+import java.sql.SQLOutput;
+
 public class Count11Demo {
+    static void main(String[] args) {
+        Count11 OBJ = new Count11();
+        System.out.println(OBJ.count11("11abc11"));
+        System.out.println(OBJ.count11("abc11x11x11"));
+        System.out.println(OBJ.count11("111"));
+
+    }
 }

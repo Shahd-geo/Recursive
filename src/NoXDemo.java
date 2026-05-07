be9c3286f1b45289547e1532c6e2a310ed3aba2e
Shahd <shahad9mubarak@gmail.com>
2026-05-07 11:29:59 +0400

    test 2

--- src/NoXDemo.java
+++ src/NoXDemo.java
@@ -2,5 +2,7 @@ public class NoXDemo {
     static void main(String[] args) {
         namedNoX obj = new namedNoX();
         System.out.println(obj.noX("xaxb"));
+        System.out.println(obj.noX("abc"));
+
     }
 }

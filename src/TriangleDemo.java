244851f7862b9d179ec6125eed9630ae3cb0f9d4
Shahd <shahad9mubarak@gmail.com>
2026-05-07 10:01:09 +0400

    test 2

--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -2,6 +2,8 @@ public class TriangleDemo {
     static void main(String[] args) {
         Triangle T=new Triangle();
         System.out.println(T.triangle(0));
+        System.out.println(T.triangle(1));
+
 
     }
 

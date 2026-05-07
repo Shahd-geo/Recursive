c3d38ff0f2f8f90fb58a9ddbd345aef121950027
Shahd <shahad9mubarak@gmail.com>
2026-05-07 09:54:27 +0400

    test 2

--- src/BunnyEarsDemo10.java
+++ src/BunnyEarsDemo10.java
@@ -2,6 +2,8 @@ public class BunnyEarsDemo10 {
     static void main(String[] args) {
         BunnyEars10 OBJ = new BunnyEars10();
         System.out.println(OBJ.bunnyEars(0));
+        System.out.println(OBJ.bunnyEars(1));
+
 
     }
 }

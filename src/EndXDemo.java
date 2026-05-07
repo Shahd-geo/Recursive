261afc06ddffbaae4a04431880581d7c30e0ccc4
Shahd <shahad9mubarak@gmail.com>
2026-05-07 11:45:03 +0400

    test 2

--- src/EndXDemo.java
+++ src/EndXDemo.java
@@ -2,6 +2,8 @@ public class EndXDemo {
     static void main(String[] args) {
         EndX obj = new EndX();
         System.out.println(obj.endX("xxre"));
+        System.out.println(obj.endX("xxhixx"));
+
 
     }
 }

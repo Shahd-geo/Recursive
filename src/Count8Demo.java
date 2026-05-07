5f067973a5fe019377cf6f750e16cb8591fbf64d
Shahd <shahad9mubarak@gmail.com>
2026-05-07 10:19:56 +0400

    Task 12: Create a Recursive Count8 Program in Java

--- src/Count8Demo.java
+++ src/Count8Demo.java
@@ -3,6 +3,8 @@ public class Count8Demo {
         Count8 OBJ = new Count8();
         System.out.println(OBJ.count8(8));
         System.out.println(OBJ.count8(818));
+        System.out.println(OBJ.count8(8818));
+
 
 
     }
